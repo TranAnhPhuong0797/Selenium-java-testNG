8942fd97f83e9d5919beb9e4065faf1f961b3256
TranAnhPhuong0797 <67380535+TranAnhPhuong0797@users.noreply.github.com>
2023-03-17 10:13:25 +0700

    Commit - Update Practice Part 6 - Handle Textbox and TextArea

--- src/Webriver_Partice_Exercise/Topic_06_Selenium_Pratice_Textbox_TextArea.java
+++ src/Webriver_Partice_Exercise/Topic_06_Selenium_Pratice_Textbox_TextArea.java
@@ -4,9 +4,10 @@ import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -17,18 +18,18 @@ public class Topic_06_Selenium_Pratice_Textbox_TextArea {
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
 	Random rand = new Random();
-	String employee_id = "AFC" + rand.nextInt(999);
+	String employee_id = "AFC" + rand.nextInt(9999);
 	String passport_num = rand.nextInt(99999) + "-" + rand.nextInt(999) + "-" + rand.nextInt(99) + "-" + rand.nextInt(9999);
 	
 	@BeforeClass
 	public void beforeClass() {
 		if (osName.contains("Windows")) {
-			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
 		} else {
-			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+			System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
 		}
 
-		driver = new FirefoxDriver();
+		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 
@@ -77,22 +78,24 @@ public class Topic_06_Selenium_Pratice_Textbox_TextArea {
 		
 		//Input valid Text box First name / Last name
 		//Step 5
-		WebElement value_firstname = driver.findElement(By.name("firstName"));
-		WebElement value_middlename = driver.findElement(By.name("middleName"));
-		WebElement value_lastname = driver.findElement(By.name("lastName"));
+		WebElement value_firstname = driver.findElement(By.xpath("//input[@name='firstName']"));
+		WebElement value_middlename = driver.findElement(By.xpath("//input[@name='middleName']"));
+		WebElement value_lastname = driver.findElement(By.xpath("//input[@name='lastName']"));
 		
 		value_firstname.sendKeys("Automation");
 		value_middlename.sendKeys("Test");
 		value_lastname.sendKeys("FC");
 
 		WebElement txt_employee = driver.findElement(By.xpath("//label[text()='Employee Id']//parent::div//following-sibling::div/input"));
-		txt_employee.clear();
+		txt_employee.sendKeys(Keys.chord(Keys.CONTROL, "a"));
+		sleepfunction(1);
+		txt_employee.sendKeys(Keys.DELETE);
 		txt_employee.sendKeys(employee_id);
 		sleepfunction(2);
 		driver.findElement(By.xpath("//p[text()='Create Login Details']/parent::div//span")).click();
 		sleepfunction(5);
 		
-		driver.findElement(By.xpath("//label[text()='Username']//parent::div//following-sibling::div//input")).sendKeys("Automation" + employee_id);
+		driver.findElement(By.xpath("//label[text()='Username']//parent::div//following-sibling::div//input")).sendKeys("Automation " + employee_id);
 		driver.findElement(By.xpath("//label[text()='Password']//parent::div//following-sibling::div//input")).sendKeys("Pass@123456");
 		driver.findElement(By.xpath("//label[text()='Confirm Password']//parent::div//following-sibling::div//input")).sendKeys("Pass@123456");
 		//Step 6
@@ -101,10 +104,10 @@ public class Topic_06_Selenium_Pratice_Textbox_TextArea {
 		
 		//Step 7
 		WebElement value_idemployee = driver.findElement(By.xpath("//label[text()='Employee Id']/parent::div//following-sibling::div/input"));		
-		
-		Assert.assertEquals(value_firstname.getAttribute("value"), "Automation");
-		Assert.assertEquals(value_middlename.getAttribute("value"), "Test");
-		Assert.assertEquals(value_lastname.getAttribute("value"), "FC");
+
+		Assert.assertEquals(driver.findElement(By.name("firstName")).getAttribute("value"), "Automation");
+		Assert.assertEquals(driver.findElement(By.name("middleName")).getAttribute("value"), "Test");
+		Assert.assertEquals(driver.findElement(By.name("lastName")).getAttribute("value"), "FC");
 		Assert.assertEquals(value_idemployee.getAttribute("value"), employee_id);
 		
 		//Step 8
@@ -128,26 +131,29 @@ public class Topic_06_Selenium_Pratice_Textbox_TextArea {
 		driver.findElement(By.xpath("//i[contains(@class,'pencil')]")).click();
 		sleepfunction(3);
 		//Step 12
-		Assert.assertEquals(txt_passportNum.getAttribute("value"), passport_num);
-		Assert.assertEquals(tat_comment.getAttribute("value"), "Automation test comment");
+		Assert.assertEquals(driver.findElement(By.xpath("//label[text()='Number']/parent::div//following-sibling::div/input")).getAttribute("value"), passport_num);
+		Assert.assertEquals(driver.findElement(By.xpath("//label[text()='Comments']/parent::div//following-sibling::div/textarea")).getAttribute("value"), "Automation test comment");
 		
 		//Step 14
 		driver.findElement(By.xpath("//li[@class='oxd-userdropdown']")).click();
 		driver.findElement(By.xpath("//a[text()='Logout']")).click();
 		sleepfunction(8);
+		
 		//Step 15
-		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Automation" + employee_id);
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Automation " + employee_id);
 		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Pass@123456");
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
 		sleepfunction(5);
+		
 		//Step 16
 		driver.findElement(By.xpath("//ul[@class='oxd-main-menu']//span[text()='My Info']")).click();
 		sleepfunction(5);
+		
 		//Step 17
-		Assert.assertEquals(value_firstname.getAttribute("value"), "Automation");
-		Assert.assertEquals(value_middlename.getAttribute("value"), "Test");
-		Assert.assertEquals(value_lastname.getAttribute("value"), "FC");
-		Assert.assertEquals(value_idemployee.getAttribute("value"), employee_id);
+		Assert.assertEquals(driver.findElement(By.name("firstName")).getAttribute("value"), "Automation");
+		Assert.assertEquals(driver.findElement(By.name("middleName")).getAttribute("value"), "Test");
+		Assert.assertEquals(driver.findElement(By.name("lastName")).getAttribute("value"), "FC");
+		Assert.assertEquals(driver.findElement(By.xpath("//label[text()='Employee Id']/parent::div//following-sibling::div/input")).getAttribute("value"), employee_id);
 		
 		//Step 18
 		driver.findElement(By.xpath("//a[text()='Immigration']")).click();
@@ -156,34 +162,9 @@ public class Topic_06_Selenium_Pratice_Textbox_TextArea {
 		sleepfunction(3);
 		
 		//Step 19
-		Assert.assertEquals(txt_passportNum.getAttribute("value"), passport_num);
-		Assert.assertEquals(tat_comment.getAttribute("value"), "Automation test comment");
+		Assert.assertEquals(driver.findElement(By.xpath("//label[text()='Number']/parent::div//following-sibling::div/input")).getAttribute("value"), passport_num);
+		Assert.assertEquals(driver.findElement(By.xpath("//label[text()='Comments']/parent::div//following-sibling::div/textarea")).getAttribute("value"), "Automation test comment");
 		
-		
-	}
-	
-	@Test
-	public void TC_03_() {
-		
-	}
-	
-	@Test
-	public void TC_04_() {
-		
-	}
-	
-	@Test
-	public void TC_05_() {
-		
-	}
-	
-	@Test
-	public void TC_06_() {
-		
-	}
-	
-	@Test
-	public void TC_07_() {
 		
 	}
 	
