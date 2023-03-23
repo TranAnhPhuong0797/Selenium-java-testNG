175891b1b9a93027a8141545533315f423a90533
TranAnhPhuong0797 <67380535+TranAnhPhuong0797@users.noreply.github.com>
2023-03-23 13:30:35 +0700

    Commit - Practice Topic 12 - Action part II

--- src/Webriver_Partice_Exercise/Topic_12__Selenium_User_Interaction_I_II_III.java
+++ src/Webriver_Partice_Exercise/Topic_12__Selenium_User_Interaction_I_II_III.java
@@ -1,9 +1,13 @@
 package Webriver_Partice_Exercise;
 
+import java.awt.RenderingHints.Key;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -53,7 +57,7 @@ public class Topic_12__Selenium_User_Interaction_I_II_III {
 	}
 	
 	@Test
-	public void TC_02() {
+	public void TC_02_hoverTootip() {
 		driver.get("http://www.myntra.com/");
 		sleepfunction(3);
 		
@@ -65,23 +69,64 @@ public class Topic_12__Selenium_User_Interaction_I_II_III {
 		Assert.assertEquals(driver.getCurrentUrl(), "https://www.myntra.com/kids-home-bath");
 	}
 	
+//	@Test
+//	public void TC_03_hoverTootip() {
+//		driver.get("https://www.fahasa.com/");
+//		sleepfunction(3);
+//		
+//		action.moveToElement(driver.findElement(By.xpath("//span[@class='icon_menu']"))).perform();
+//		sleepfunction(1);
+//		
+//		action.moveToElement(driver.findElement(By.xpath("//a[@title='Sách Trong Nước']"))).perform();
+//		sleepfunction(2);
+//		
+//		action.moveToElement(driver.findElement(By.xpath("//div[contains(@class,'menu_content')]//span[text()='TIỂU SỬ - HỒI KÝ']//ancestor::h3//following-sibling::ul//a[text()='Kinh Tế']"))).perform();
+//		sleepfunction(2);
+//	}
+	
 	@Test
-	public void TC_03() {
-		driver.get("https://www.fahasa.com/");
-		sleepfunction(3);
-		
-		action.moveToElement(driver.findElement(By.xpath("//span[@class='icon_menu']"))).perform();
-		sleepfunction(1);
-		
-		action.moveToElement(driver.findElement(By.xpath("//a[@title='Sách Trong Nước']"))).perform();
+	public void TC_04_ClickandHold() {
+		driver.get("https://automationfc.github.io/jquery-selectable/");
 		sleepfunction(2);
 		
-		action.moveToElement(driver.findElement(By.xpath("//div[contains(@class,'menu_content')]//span[text()='TIỂU SỬ - HỒI KÝ']//ancestor::h3//following-sibling::ul//a[text()='Kinh Tế']"))).perform();
+		List<WebElement> listNumber = driver.findElements(By.xpath("//ol[@id='selectable']//li"));
+		
+		action.clickAndHold(listNumber.get(0))
+		.moveToElement(listNumber.get(3))
+		.release()
+		.perform();
+		
 		sleepfunction(2);
+		
+		List<WebElement> listSelectedNumber = driver.findElements(By.cssSelector("ol#selectable>li.ui-selected"));
+		
+		Assert.assertEquals(listSelectedNumber.size(), 4);
+	}
+	
+	@Test
+	public void TC_05_ClickandSelected() {
+		driver.get("https://automationfc.github.io/jquery-selectable/");
+		sleepfunction(2);
+		
+		
+		List<WebElement> listNumber = driver.findElements(By.xpath("//ol[@id='selectable']//li"));
+		
+		//click ctrl down
+		action.keyDown(Keys.CONTROL).perform();
+		
+		//Click random
+		action.click(listNumber.get(0))
+		.click(listNumber.get(5))
+		.click(listNumber.get(7)).perform();
+		sleepfunction(2);
+		
+		List<WebElement> listSelectedNumber = driver.findElements(By.cssSelector("ol#selectable>li.ui-selected"));
+		
+		Assert.assertEquals(listSelectedNumber.size(), 3);		
 	}
 	
 	@AfterClass
 	public void afterClass() {
-		driver.quit();
+		//driver.quit();
 	}
 }
